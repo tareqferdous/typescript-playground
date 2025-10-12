5f57d46e0a1529c48f71c4f9b61c8b91983eb187
tareqferdous <16101017@uap-bd.edu>
2025-10-12 21:45:54 +0600

    explore array in ts

---
+++ array/index.ts
@@ -0,0 +1,11 @@
+let listOfFriends: string[] = ["Alice", "Bob", "Charlie"];
+let listOfScores: number[] = [85, 90, 78];
+let listOfMixed: (string | number)[] = ["Alice", 85, "Bob", 90];
+let listOfBooleans: boolean[] = [true, false, true];
+
+// 2d array
+let matrix: number[][] = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
